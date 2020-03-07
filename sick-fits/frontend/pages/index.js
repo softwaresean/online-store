b175cca57db7d3d02e425fef3bb18d7610e04654
seant@bgsu.edu <54330446+softwaresean@users.noreply.github.com>
2020-03-07 12:38:56 -0500

    Added selling page routing.

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import Link from 'next/link';
 const Home = props => (
-    <p>Hey!</p>
+    <div>
+        <p>Hey!</p>
+        <Link href="/sell">
+            <a>Sell!</a>
+        </Link>
+    </div>
 )
 
 export default Home;
